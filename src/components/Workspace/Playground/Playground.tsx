0090e6d615a1fea8cce40415494fd550831ad1e9
Hoshino Ai <156778915+Trungdoan2009@users.noreply.github.com>
2024-08-08 09:58:04 +0700

    Update Playground.tsx

--- src/components/Workspace/Playground/Playground.tsx
+++ src/components/Workspace/Playground/Playground.tsx
@@ -84,7 +84,7 @@ const Playground: React.FC<PlaygroundProps> = ({ problem, setSuccess, setSolved 
         } catch (error: any) {
             console.log(error.message);
             if (error.message.startsWith("Sai testcase")) {
-                toast.error(error.message, 'bạn đã sai testcases' {
+                toast.error(error.message, 'bạn đã sai testcases' ,  {
                     position: "top-center",
                     autoClose: 3000,
                 });
